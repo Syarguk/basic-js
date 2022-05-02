b3b7b64c6717b8170c93b3c9a90b7c8131d212d8
Syarguk <bulbaburak@gmail.com>
2022-05-02 22:55:06 +0300

    feat: dsf

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum( matrix ) {
+  let resSum = 0, posNulls = [];
+  for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+          if (matrix[i][j] == 0) {
+              posNulls.push(j);
+          } else if (!posNulls.includes(j)) {
+              resSum += matrix[i][j];
+          }
+      }
+  }
+  return resSum;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+    if (!Array.isArray(arr)) return "'arr' parameter must be an instance of the Array!";
+    let resArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') resArr.push(arr[i]);
+        if(arr[i] == '--discard-prev') {
+            resArr.pop();
+        } else if (arr[i] == '--double-prev') {
+            if (arr[i - 1]) resArr.push(arr[i - 1]);
+        } else if (arr[i] == '--double-next') {
+            if (arr[i + 1]) resArr.push(arr[i + 1]);
+        } else if (arr[i] == '--discard-next') {
+            if (arr[i + 1]) {i = i + 1; arr[i + 1] = undefined;}
+        }
+    }
+    return resArr;
 }
 
 module.exports = {
